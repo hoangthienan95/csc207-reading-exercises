1a327071eadaed8765f186c72e7b8812a0f244f7
An Hoang <An@Ans-MacBook-Pro.local>
2017-02-20 13:36:26 -0600

    2-20-2017

---
+++ LinkedList.java
@@ -0,0 +1,17 @@
+
+
+public class LinkedList {
+	private Node first;
+	
+	public LinkedList(){
+		setFirst(NULL);
+	}
+
+	public Node getFirst() {
+		return first;
+	}
+
+	public void setFirst(Node first) {
+		this.first = first;
+	}
+}
